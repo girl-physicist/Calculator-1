f3b5b6d20a534a5d5ac9b332bd44192518e282ef
Anastasia <girl-physicist@mail.ru>
2023-09-26 15:54:44 +0300

    comments

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,15 +6,15 @@ internal class Program
     private static void Main(string[] args)
     {
         double result;
-        char a;
+        char a; // следует давать значимые (передающие их смысл) имена переменным, например typeOfTransaction
         char again;
-        link1:
+        link1: // использование якорей не приветствуется
         Console.WriteLine("Введите первое число: ");
-        double num1 = Convert.ToDouble(Console.ReadLine());
+        double num1 = Convert.ToDouble(Console.ReadLine()); // следовало добавить обработчик исключений (если не удалось преобразовать строку в число)
         Console.WriteLine("Введите способ вычислений(+,-,*,/): ");
-        a = Convert.ToChar(Console.ReadLine());
+        a = Convert.ToChar(Console.ReadLine()); // следовало добавить обработчик исключений (если не удалось преобразовать строку в число)
         Console.WriteLine("Введите второе число: ");
-        double num2 = Convert.ToDouble(Console.ReadLine());
+        double num2 = Convert.ToDouble(Console.ReadLine()); // следовало добавить обработчик исключений (если не удалось преобразовать строку в число)
         switch(a)
         {
             case '+':
@@ -30,7 +30,7 @@ internal class Program
                 Console.WriteLine($"Ответ: {num1} * {num2} = {result}");
                 break;
             case '/':
-                result = num1 / num2;
+                result = num1 / num2; // нужна проверка деления на 0
                 Console.WriteLine($"Ответ: {num1} / {num2} = {result}");
                 break;
             default: 
@@ -38,9 +38,9 @@ internal class Program
                 break;
         }
         Console.WriteLine("Желаете продолжить?(y/n)");
-        again = Convert.ToChar(Console.ReadLine());
+        again = Convert.ToChar(Console.ReadLine()); // следовало добавить обработчик исключений (если не удалось преобразовать строку в символ)
         if (again == 'y') goto link1; 
-        else goto link2;
+        else goto link2; // не используйте якеоря!!! говорила об этом на лекции
         link2:
         Console.WriteLine("Завершение программы.");
     }
